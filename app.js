b0c8db55d560bf5a291f6ddbe3b8bb0c36f956f8
aman tiwari <amantiwari78632@gmail.com>
2021-03-26 23:37:28 +0530

    added image upload api and handle error

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 import express from "express";
 import feed from "./routes/feed";
+import auth from "./routes/auth";
 import mongoose from "mongoose";
 import path from "path";
 require("dotenv").config();
 
+
+import multer from "multer";
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null,  Date.now() + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 const app = express();
 app.use(express.json());
-// app.use(
-//   express.urlencoded({
-//     extended: true,
-//   })
-// );
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 
-app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -27,6 +48,16 @@ app.use((req, res, next) => {
 // app.use('images', )
 
 app.use("/feed", feed);
+app.use("/auth", auth);
+
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data
+  res.status(status).json({ message: message , data: data });
+});
 const uri = process.env.MONGO_URI;
 mongoose
   .connect(uri)
