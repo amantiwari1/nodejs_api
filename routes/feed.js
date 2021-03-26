5bcd868eb74f6e1ad4ab26a3095c7db0e7bcf4f3
aman tiwari <amantiwari78632@gmail.com>
2021-03-26 23:40:20 +0530

    added new  CRUD routes

--- routes/feed.js
+++ routes/feed.js
@@ -1,21 +1,25 @@
 import express, { Router } from "express";
 import * as feedControllers from "../controllers/feed";
 import { check } from "express-validator";
+import isAuth from '../middleware/is-auth'
 
 
 const updateArticleValidations = [
     check("title", "Please Enter title").not().isEmpty(),
     check("content", "Please Enter content").not().isEmpty(),
     check("title", "max 150 characters").isLength({ max: 150 }),
-    check("title", "max 150 characters").isLength({ min: 5 }),
-    check("content", "too short content").isLength({ min: 7 }),
+    check("title", "max 150 characters").isLength({ min: 7 }),
+    check("content", "too short content").isLength({ min: 5 }),
     check("content", "too long content").isLength({ max: 500 }),
 ];
 
 
 const feed = Router();
 
-feed.route('/posts').get(feedControllers.getPosts)
-feed.route('/post').post(updateArticleValidations, feedControllers.createPosts)
+feed.route('/posts').get(isAuth, feedControllers.getPosts)
+feed.route('/post').post(isAuth, updateArticleValidations, feedControllers.createPosts)
+feed.route('/post/:postId').get(isAuth, feedControllers.getPost)
+feed.route('/post/:postId').put(isAuth, updateArticleValidations, feedControllers.updatePost)
+feed.route('/post/:postId').delete(isAuth, feedControllers.deletePost)
 
 export default feed;
